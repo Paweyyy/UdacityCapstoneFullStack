75a84393bcb45836479069e3e28ad8aadf2d0af1
Paweyyy <bang7799@gmail.com>
2022-01-02 19:02:14 +0100

    First Commit

---
+++ models.py
@@ -0,0 +1,57 @@
+import os
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.environ['DATABASE_URL']
+if database_path.startswith("postgres://"):
+  database_path = database_path.replace("postgres://", "postgresql://", 1)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+'''
+Person
+Have title and release year
+Actor 
+Actors with attributes name, age and gender
+'''
+class Actor(db.Model):
+  __tablename__ = 'actors'
+
+  id = Column(db.Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  age = Column(db.Integer, nullable=False)
+  gender = Column(String, nullable=False)
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'age': self.age,
+      'gender': self.gender
+    }
+
+class Movie(db.Model):  
+  __tablename__ = 'movies'
+
+  id = Column(db.Integer, primary_key=True)
+  title = Column(String, nullable=False)
+  release_date = Column(db.Datetime, nullable=False)
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date
+      }
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://postgres:paul99@localhost:5432/postgres"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
